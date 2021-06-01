67e94e1035571ca4aee386874e49a825f4e051e8
Vineet <vineet98love@gmail.com>
2021-06-01 17:50:41 +0530

    modification

--- block-BJaadl/code/index.js
+++ block-BJaadl/code/index.js
@@ -13,8 +13,8 @@ let words = [
 // (Use above array "words" to test it). If there are 2 with the same length, 
 // it should return the first occurrence.
 
-function findLongestWord(words){
-   
+function findLongestWord(array){
+    return [...words].sort((a,b) => a.length - b.length).pop();
 }
 
 
@@ -23,20 +23,34 @@ var wordsLength =  words.map(elements => elements.length);
 
 // - Create a new array that only contains word with atleast one vowel.
 
+function checkVowel(words){
+  return ( words.toLowerCase().includes('a') ||
+         words.toLowerCase().includes('e') ||
+         words.toLowerCase().includes('i') ||
+         words.toLowerCase().includes('o') ||
+         words.toLowerCase().includes('u') 
+  ); 
+}
+let vowelFilter = words.filter((w) => checkVowel(w));
+
 // - Find the index of the word "rhythm"
  words.lastIndexOf("rhythm");
 // - Create a new array that contians words not starting with vowel.
 
-var notStartVowel = words.filter((word) => word.notstartsWith("a"));
+var notStartVowel = words.filter((word) => !checkVowel(word[0]));
 
 // - Create a new array that contians words not ending with vowel
+let notEndingWithVowel = words.filter((word) => !checkVowel(w[w.length - 1]));
 
 let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 // - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers
 
-function sumArray(numbers){
-    
+function sumArrays(numbers){
+  return array.reduce((acc, cv) => {
+    acc = acc + cv;
+    return acc;
+  }, 0);
 }
 // - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...]
 
@@ -61,7 +75,15 @@ Yes, it mutate the originals array. That wgy we are using spread operator
 let sumArray = numbers.reduce(function(accumulator, currentValue) {
   return accumulator + currentValue
 });
+
 //- Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers
+
+function avarageNumber(){
+  return  array.reduce((acc, cv) => {
+      acc = acc + cv;
+      return acc;
+  },0) / array.length;
+}
 
 let strings = [
   'seat',
@@ -77,3 +99,14 @@ let strings = [
 ];
 
 // - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
+
+function averageWordLength(words){
+  return ( 
+    words
+    .map((w) => w.length)
+    .reduce((acc, cv) => {
+      acc = acc + cv
+      return acc + cv
+    },0)/ words.length
+  );
+}
